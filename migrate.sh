c5d261c692193fd6f01a4af06dcf0395f1c5c4dd
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2018-05-30 19:57:07 -0300

    Updated to run on Linux and ask for user/pass

--- migrate.sh
+++ migrate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
+read -p "Digite o usuario:" -s user
+read -p "Digite a senha:" -s pass
 workdir=$(pwd)
 
-base_url='https://gitlab.com/aovs/apostilas'
-find ../apostilas-novas -path "*" -type d -depth 1  > diretorios_completos.txt
+base_url='gitlab.com/aovs/apostilas'
+find ../apostilas-novas -maxdepth 1 -type d > diretorios_completos.txt
 
 while read -r dir
 do
@@ -29,7 +30,7 @@ do
 		git prune
 		git remote rm origin
 
-		git push -u  $base_url/$current.git master
+		git push -u  https://$user:$pass@$base_url/$current.git master
 
 		cd $workdir
 	
